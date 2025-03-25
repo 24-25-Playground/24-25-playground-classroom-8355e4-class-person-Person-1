774d04e11328de11c0c03e3916adec7ab0e2e5df
w.kaiser@htl-grieskirchen.at <w.kaiser@htl-grieskirchen.at>
2025-03-25 20:23:45 +0100

    Assignment done

--- src/main/java/net/htlgrieskirchen/pos/classroom/Person.java
+++ src/main/java/net/htlgrieskirchen/pos/classroom/Person.java
@@ -4,8 +4,14 @@ public class Person {
     private final String name;
     private final String firstname;
 
+    public Person(String name, String firstname) {
+        this.name = name;
+        this.firstname = firstname;
+    }
+
     @Override
     public String toString() {
         // add code to return firstname, a blank and then name
+        return firstname + " " + name;
     }
 }
